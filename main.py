b0cb0d68e8a094ac036ae5a34f6f239377337cf9
xcodecat <xmissh0lyc4t@gmail.com>
2023-07-25 19:59:37 +0200

    refactor(main.py): cleanup code
    - removed if-statement for time period
    - removed if-statement for file deletion
    - added dict for time convertion

--- main.py
+++ main.py
@@ -2,44 +2,27 @@ import os
 import time
 import shutil
 
-path = input("Enter a valid absolut path (e.g. /home/xcodecat/Downloads/): ")
-now = time.time()
 
-time_period = input("Enter desired time period (e.g. days, hours, minutes): ")
+def main():
+    path = input("Enter a valid absolut path (defaults to $HOME/Downloads/): ") or os.getenv("HOME") + "/Downloads/"
+    now = time.time()
+    times = {'days': 86400, 'hours': 3600, 'minutes': 60}
+    time_period = input("Enter desired time period (e.g. days, hours, minutes): ")
 
-if time_period == "days":
-    duration_text = "days"
-elif time_period == "hours":
-    duration_text = "hours"
-elif time_period == "minutes":
-    duration_text = "minutes"
-else:
-    print("Invalid time period entered. Please choose from 'days', 'hours', or 'minutes'!")
-    exit()
+    if time_period not in times:
+        print("Invalid time period entered. Please choose from 'days', 'hours', or 'minutes'!")
+        exit()
 
-duration = int(input(f"Enter the number of {duration_text} for considering file deletion (e.g. 7): "))
+    duration = int(input(f"Enter the number of {time_period} for considering file deletion (e.g. 7): "))
 
-if time_period == "days":
     for f in os.listdir(path):
         f = os.path.join(path, f)
-        if os.stat(f).st_mtime < now - duration * 86400:
+        if os.stat(f).st_mtime < now - duration * times.get(time_period):
             if os.path.isfile(f):
                 os.remove(os.path.join(path, f))
             elif os.path.isdir(f):
                 shutil.rmtree(f)
-elif time_period == "hours":
-    for f in os.listdir(path):
-        f = os.path.join(path, f)
-        if os.stat(f).st_mtime < now - duration * 3600:
-            if os.path.isfile(f):
-                os.remove(os.path.join(path, f))
-            elif os.path.isdir(f):
-                shutil.rmtree(f)
-elif time_period == "minutes":
-    for f in os.listdir(path):
-        f = os.path.join(path, f)
-        if os.stat(f).st_mtime < now - duration * 60:
-            if os.path.isfile(f):
-                os.remove(os.path.join(path, f))
-            elif os.path.isdir(f):
-                shutil.rmtree(f)
+
+
+if __name__ == "__main__":
+    main()
